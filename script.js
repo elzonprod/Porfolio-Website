218488ce3a0ab1d6202d0015b667c6a538620152
Dmitriy <dmitriy.timoshchenko@gmail.com>
2022-04-06 13:14:39 +0200

    Create Main function for navbar / add smooth effect / work with Offset

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Start project')
+// Fixed navbar
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section')
+const navbarLinks = document.querySelectorAll('.navbar-link')
+const progress = document.querySelector('.progress-bars-wrapper')
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50]
+
+window.addEventListener('scroll', () => {
+    mainFn()
+})
+
+const mainFn = () => {
+    if (window.pageYOffset >= navbarOffsetTop) {
+        navbar.classList.add('sticky')
+        console.log('sticky')
+    } else {
+        navbar.classList.remove('sticky')
+        console.log('remove stciky')
+    }
+
+    sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+            navbarLinks.forEach(NavbarLink => {
+                NavbarLink.classList.remove('change')
+            })
+            navbarLinks[i].classList.add('change');
+        }
+    })
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll('.progress-percent').forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+            el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i]
+        })
+    }
+}
